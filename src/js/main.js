831fc77bf62d4716aeab7827dcc2bbe0b263423d
Daria <dariadaniv@gmail.com>
2025-05-23 11:43:29 +0200

    Adding main.js

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+async function loadComponents() {
+  // Логика открытия/закрытия меню после загрузки хедера
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  if (refs.openMenuBtn && refs.closeMenuBtn && refs.menu) {
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-hidden');
+      document.body.classList.toggle('no-scroll');
+    }
+  } else {
+    console.error('Не найдены элементы меню:', refs);
+  }
+}
+
+loadComponents();
