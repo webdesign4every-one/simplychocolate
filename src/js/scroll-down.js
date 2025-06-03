a3bf959edaac1cbf5eaeb90e64dac6a96ff6b139
Daria <dariadaniv@gmail.com>
2025-06-03 17:22:42 +0200

    Update scroll

--- src/js/scroll-down.js
+++ src/js/scroll-down.js
@@ -22,3 +22,42 @@ document.querySelector('.how-scroll').addEventListener('click', function (e) {
     behavior: 'smooth',
   });
 });
+
+//our
+document.querySelector('.our-scroll').addEventListener('click', function (e) {
+  e.preventDefault(); // предотвращает переход по ссылке, если это <a>
+
+  const element = document.querySelector('#our');
+  const topPosition = element.getBoundingClientRect().top + window.pageYOffset;
+
+  window.scrollTo({
+    top: topPosition,
+    behavior: 'smooth',
+  });
+});
+
+//top
+document.querySelector('.top-scroll').addEventListener('click', function (e) {
+  e.preventDefault(); // предотвращает переход по ссылке, если это <a>
+
+  const element = document.querySelector('#top');
+  const topPosition = element.getBoundingClientRect().top + window.pageYOffset;
+
+  window.scrollTo({
+    top: topPosition,
+    behavior: 'smooth',
+  });
+});
+
+//review
+document.querySelector('.review-scroll').addEventListener('click', function (e) {
+  e.preventDefault(); // предотвращает переход по ссылке, если это <a>
+
+  const element = document.querySelector('#review');
+  const topPosition = element.getBoundingClientRect().top + window.pageYOffset;
+
+  window.scrollTo({
+    top: topPosition,
+    behavior: 'smooth',
+  });
+});
